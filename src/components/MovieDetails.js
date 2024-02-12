7fbcff61291d551aaa4e9e4345d40eab54b2cef7
Shwetha-703 <shweta.shingare11@gmail.com>
2024-02-12 12:08:28 +0530

    fixing mobile view issue with movie details

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -5,12 +5,12 @@ const MovieDetails = ({movie}) => {
     
     const poster_path = movie.poster_path;
   return (
-    <div className='flex justify-center bg-black m-auto'>
+    <div className='flex flex-wrap justify-center bg-black m-auto'>
       <img className='' src={TMDB_IMG_URL+poster_path}/>
       <div className=''>
-        <h1 className='p-4 text-2xl font-semibold'>{movie.title}</h1>
-        <h2 className='p-4 text-xs md:text-md'>{movie.overview}</h2>
-        <h2 className='p-4 text-xs md:text-md'>Release date : {movie.release_date}</h2>
+        <h1 className='p-1 md:p-4 text-sm md:text-2xl font-semibold'>{movie.title}</h1>
+        <h2 className='p-1 md:p-4 text-xs md:text-md'>{movie.overview}</h2>
+        <h2 className='p-1 md:p-4 text-xs md:text-md'>Release date : {movie.release_date}</h2>
 
       </div>
     </div>
